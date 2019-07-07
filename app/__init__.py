e7ea1f6d07a1045b72cc43a2af74f13634e95f05
Francis Mukuha <mukuha58@gmail.com>
2019-07-07 19:49:20 +0300

    add login and logout

--- app/__init__.py
+++ app/__init__.py
@@ -32,7 +32,7 @@ def create_app(config_name):
     # Initialising flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
-    # login_manager.init_app(app)
+    login_manager.init_app(app)
 
     # Regestering the main blueprint
     from .main import main as main_blueprint
