ef9a21c46dc1eb5d2cae5b671c091fa76c8ab2c5
Francis Mukuha <mukuha58@gmail.com>
2019-07-08 07:49:13 +0300

    add api key for random quotes

---
+++ app/main/quote.py
@@ -0,0 +1,10 @@
+class Quote:
+    '''
+    Quote class to define Movie Objects
+    '''
+
+    def __init__(self,id,author,quote,vote_average,vote_count):
+        self.id =id
+        self.author = author
+        self.quote = quote
+        
